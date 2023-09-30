2939e794014d6efdac7d712eb4643fbb502a8e2b
Dell <m.mahmoud1101990@gmail.com>
2023-09-30 05:22:51 -0700

    Button and popul added

--- app/src/main/java/com/example/lape2/MainActivity.java
+++ app/src/main/java/com/example/lape2/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.lape2;
 
+import static android.app.ProgressDialog.show;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void en8techangetextclick(View view) {
+        Toast.makeText (this, "Some message", Toast.LENGTH_LONG).show();
     }
 }
